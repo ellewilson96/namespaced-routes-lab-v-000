36dcb3082d420a1ac68e4c3376f777ef6f009fe1
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-18 19:26:18 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,13 +1,2 @@
 class Preference < ActiveRecord::Base
-  def artist_sort_order(artist)
-  end 
-
-  def artist_sort_order=(artist)
-  end
-
-  def song_sort_order(song)
-  end 
-
-  def song_sort_order=(song)
-  end
 end

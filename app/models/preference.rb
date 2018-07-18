e61ff3423850faf7d6ec0e7e9b62378c22e14f1d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-18 19:20:13 +0000

    automatically backed up by learn

---
+++ app/models/preference.rb
@@ -0,0 +1,13 @@
+class Preference < ActiveRecord::Base
+  def artist_sort_order(artist)
+  end 
+
+  def artist_sort_order=(artist)
+  end
+
+  def song_sort_order(song)
+  end 
+
+  def song_sort_order=(song)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+scope '/admin' do
+  resources :preferences, only: [:index, :new]
+end
+
   resources :artists do
-    resources :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show, :new]
   end
   resources :songs
 

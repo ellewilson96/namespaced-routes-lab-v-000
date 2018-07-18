ba1978fab88b7734b4e81d098bda0caf520647ff
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-18 19:28:59 +0000

    automatically backed up by learn

--- db/migrate/20180718192756_add_fields_to_preferences.rb
+++ db/migrate/20180718192756_add_fields_to_preferences.rb
@@ -1,7 +1,7 @@
 class AddFieldsToPreferences < ActiveRecord::Migration
   def change
-    add_column :preferences, :artist_sort_order, :string 
-    add_column :preferences, :song_sort_order, :string 
+    add_column :preferences, :artist_sort_order, :string
+    add_column :preferences, :song_sort_order, :string
     add_column :preferences, :allow_create_artists, :boolean
     add_column :preferences, :allow_create_songs, :boolean
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180718190742) do
+ActiveRecord::Schema.define(version: 20180718192756) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20180718190742) do
   end
 
   create_table "preferences", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "artist_sort_order"
+    t.string   "song_sort_order"
+    t.boolean  "allow_create_artists"
+    t.boolean  "allow_create_songs"
   end
 
   create_table "songs", force: :cascade do |t|
